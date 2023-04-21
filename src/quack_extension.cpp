a2ce21148df818ce16dc01d5244a105734aea60d
Carlo Piovesan <piovesan.carlo@gmail.com>
2023-04-21 16:25:55 +0200

    Experimental replacement scan

--- src/quack_extension.cpp
+++ src/quack_extension.cpp
@@ -18,6 +18,36 @@ inline void QuackScalarFun(DataChunk &args, ExpressionState &state, Vector &resu
 	    [&](string_t name) { 
 			return StringVector::AddString(result, "🐥 🐥 🐥 Quack "+name.GetString()+" 🐥 🐥 🐥");;
         });
+
+
+unique_ptr<TableRef> QuackzScanReplacement(ClientContext &context, const string &table_name,
+                                            ReplacementScanData *data) {
+	auto lower_name = StringUtil::Lower(table_name);
+	if (!StringUtil::EndsWith(lower_name, ".quack")) {
+		return nullptr;
+	}
+
+		// This to be refactored away
+		char *str = (char *)EM_ASM_PTR({
+			console.log(UTF8ToString($0));
+			var jsString = "https://quaaack.org/" + UTF8ToString($0) + ".csv";
+			var lengthBytes = lengthBytesUTF8(jsString) + 1;
+			// 'jsString.length' would return the length of the string as UTF-16
+			// units, but Emscripten C strings operate as UTF-8.
+			var stringOnWasmHeap = _malloc(lengthBytes);
+			stringToUTF8(jsString, stringOnWasmHeap, lengthBytes);
+			return stringOnWasmHeap;
+		}, table_name.c_str());
+		std::string address(str);
+		free(str);
+
+		string new_table_name = address;
+
+	auto table_function = make_uniq<TableFunctionRef>();
+	vector<unique_ptr<ParsedExpression>> children;
+	children.push_back(make_uniq<ConstantExpression>(Value(new_table_name)));
+	table_function->function = make_uniq<FunctionExpression>("read_csv_auto", std::move(children));
+	return std::move(table_function);
 }
 
 static void LoadInternal(DatabaseInstance &instance) {
@@ -31,6 +61,8 @@ static void LoadInternal(DatabaseInstance &instance) {
     quack_fun_info.on_conflict = OnCreateConflict::ALTER_ON_CONFLICT;
     catalog.CreateFunction(*con.context, &quack_fun_info);
     con.Commit();
+	auto &config = DBConfig::GetConfig(*db.instance);
+	config.replacement_scans.emplace_back(QuackzScanReplacement);
 }
 
 void QuackExtension::Load(DuckDB &db) {
